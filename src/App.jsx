9c11248065a823973bc16f32d5d580a5688824c8
Valentina Otočan <valentinaotocan@gmail.com>
2024-03-27 19:54:35 +0100

    Implement Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import RandomList from "./pages/RandomList";
+import Waves from "./components/Waves";
 function App() {
   return (
     <>
+      <Waves rotate={180}/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/random-list" element={<RandomList />} />
       </Routes>
+      <Waves rotate={0}/>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <Link to="/random-list" className="underline">
+        Random Number List{" "}
+        <span className="right-arrow">
+          <FaArrowRight />
+        </span>
+      </Link>
+    </div>
+  );
 }
 export default Home
