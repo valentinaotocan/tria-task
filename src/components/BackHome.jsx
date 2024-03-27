4dd268855f9e3153981c89b05c4f4fe406bd920b
Valentina Otočan <valentinaotocan@gmail.com>
2024-03-27 20:37:32 +0100

    Add Icon - Left Arrow

--- src/components/BackHome.jsx
+++ src/components/BackHome.jsx
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 function BackHome() {
   return (
     <div className="back-home">
-     <Link to="/" className="underline">Home</Link>
+      <Link to="/" className="underline">
+        <span className="left-arrow">
+          <FaArrowLeft />
+        </span>
+        Home
+      </Link>
     </div>
-  )
+  );
 }
 export default BackHome
